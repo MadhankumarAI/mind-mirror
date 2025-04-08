24c048a208c9478503cfa26e1efd89b6c9d3f6f3
MadhankumarAI <jaip90080@gmail.com>
2025-04-08 15:55:57 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -26,7 +26,7 @@ const App = () => {
     if (!image) return;
 
     try {
-      const response = await fetch('http://localhost:5000/detect_emotion', {
+      const response = await  fetch('/.netlify/functions/detect_emotion', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ image }),
